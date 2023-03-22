d18555d4d4f39b19a5385c62623a85245e66beea
Vitor Adriano <112006474+Rybaksz@users.noreply.github.com>
2023-03-22 16:54:56 -0300

    Add files via upload

---
+++ 09-idade.js
@@ -0,0 +1,23 @@
+
+let idade = 17;
+
+if (idade >= 18) {
+    console.log(" A pessoa tem:" ,  idade );
+    console.log("Você é maior de idade");
+
+}
+
+
+else {
+
+    console.log("A pessoa tem :",  idade)
+    console.log("Menor de 18 anos")
+
+}
+
+
+
+
+
+
+
